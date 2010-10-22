43256c1b242539cd1608d1e0810d40564874cb96
Seppo <info@dr-barke-immobilien.de>
2010-10-22 13:25:44 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drbi::Application.config.secret_token = '6408f79e0d84b74646818cd8c15b027a1a194d2e9629407f457fa386127a3bfbc9422d805d26c6d671ec77735311dcb4b9a2381320a8aba477409904befadbf9'
